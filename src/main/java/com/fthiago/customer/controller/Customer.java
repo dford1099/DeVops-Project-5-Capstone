afee58d75e3cda5260000d8aec882be8264cb031
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-05-02 23:53:03 -0300

    feature: including request tests into project folder

---
+++ src/main/java/com/fthiago/customer/controller/Customer.java
@@ -0,0 +1,27 @@
+package com.fthiago.customer.controller;
+
+public class Customer {
+    private long id;
+    private String name;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
