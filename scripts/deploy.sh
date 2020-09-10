f5667f2d02eb0ea7e0594d25ae6ed4f0e45711e6
Davone <davone.ford@gmail.com>
2020-09-10 18:46:38 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,2 @@
-docker --version && docker build -t spring-tdd . && docker tag spring-tdd:latest 156790592858.dkr.ecr.us-west-2.amazonaws.com/spring-tdd:latest && docker images
+#docker --version && docker build -t spring-tdd . && docker tag spring-tdd:latest 156790592858.dkr.ecr.us-west-2.amazonaws.com/spring-tdd:latest && docker images
+docker --version && docker build -t 156790592858.dkr.ecr.us-west-2.amazonaws.com/spring-tdd:latest -f ../../Dockerfile . && docker push 156790592858.dkr.ecr.us-west-2.amazonaws.com/spring-tdd:latest && docker images
